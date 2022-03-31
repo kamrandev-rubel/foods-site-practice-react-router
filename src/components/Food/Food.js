4692d45ce7a7b496f2a3001b3e8a15f2dde14ca0
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-03-31 11:05:27 +0300

    added restaurant components

---
+++ src/components/Food/Food.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Food = (props) => {
+    const {}= props.food
+    return (
+        <div>
+            <img src="" alt="" />
+        </div>
+    );
+};
+
+export default Food;
---
+++ src/components/Restaurant/Restaurant.js
@@ -0,0 +1,31 @@
+import { data } from 'autoprefixer';
+import React, { useEffect, useState } from 'react';
+import Food from '../Food/Food';
+
+const Restaurant = () => {
+    const [inputValue, setInputValue] = useState('')
+    const [foods, setFoods] = useState([])
+    const searchBox = e => {
+        setInputValue(e.target.value)
+    }
+    useEffect(() => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
+            .then(res => res.json)
+            .then(data => setFoods(data))
+    },[])
+    
+    return (
+        <div>
+            <h2 className='text-5xl font-bold text-indigo-500 mt-10 mb-5'>Find the food you want</h2>
+            <input onChange={searchBox} type="text" placeholder='Search Foods' className='w-6/12 h-10 p-3 py-5 focus:text-2xl outline-none border-4 border-indigo-500 rounded-lg' />
+            
+            <dir>
+                {
+                    foods.map(food => <Food key={food.idMeal} food={food} />)
+                }
+            </dir>
+        </div>
+    );
+};
+
+export default Restaurant;
